28b6311418be524b850ddf90b1fff390b82a58f1
Leonid <Anevskyi2018@yandex.ru>
2022-11-07 22:26:26 +0400

    add argparse module to read from cmd

--- Task6.py
+++ Task6.py
@@ -1,4 +1,5 @@
 import concurrent.futures as pool
+import argparse
 
 # Задание 6. Используя модуль concurrent.futures, распараллельте на процессы процедуру вычисления выборочной дисперсии трех чисел. 
 # Написанное приложение должно быть консольным. 
@@ -18,15 +19,17 @@ def get_variance(n1: float, n2: float, n3: float) -> float:
 
 
 if __name__ == "__main__":
-    n1 = float(input('Input first number: '))
-    n2 = float(input('Input second number: '))
-    n3 = float(input('Input third number: '))
-    mean = get_mean(n1, n2, n3)
+    parser = argparse.ArgumentParser(description='variance from numbers')
+    parser.add_argument('n1', type=float, help='first number')
+    parser.add_argument('n2', type=float, help='second number')
+    parser.add_argument('n3', type=float, help='third number')
+    args = parser.parse_args()
+    mean = get_mean(args.n1, args.n2, args.n3)
     with pool.ThreadPoolExecutor(max_workers=4) as executer:
-        sd1 = executer.submit(get_mean, n1, n2, n3)
-        sd2 = executer.submit(get_square_devitation, n1, mean)
-        sd3 = executer.submit(get_square_devitation, n2, mean)
-        sd4 = executer.submit(get_square_devitation, n3, mean)
+        #sd1 = executer.submit(get_mean, n1, n2, n3)
+        sd2 = executer.submit(get_square_devitation, args.n1, mean)
+        sd3 = executer.submit(get_square_devitation, args.n2, mean)
+        sd4 = executer.submit(get_square_devitation, args.n3, mean)
         result = executer.submit(get_variance, float(sd2.result()), float(sd3.result()), float(sd4.result()) )
         print(result.result())
 
